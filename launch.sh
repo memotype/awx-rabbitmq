25cce3d671adc2a82e8bc3d4e44a8a9c15e92f9f
Isaac Freeman <ifreeman@cree.com>
2018-07-19 15:44:28 -0400

    Support RABBITMQ_NODENAME_PREFIX as envVar
    kubectl deployment.yml files don't appear to support referencing
    valueFrom style envVars in the value for other envVars. See
    corresponding PR for main AWX git repo (will add reference after
    it's submitted)

--- launch.sh
+++ launch.sh
@@ -3,13 +3,19 @@
 [[ -n "$DEBUG" ]] && set -x
 set -e
 
-if [ `id -u` -ge 500 ]; then
-    echo "rabbitmq:x:`id -u`:`id -g`:,,,:${HOME}:/bin/ash" >> /tmp/passwd
-    cat /tmp/passwd > /etc/passwd
-    rm /tmp/passwd
+if [[ `id -u` -ge 500 ]]; then
+    adduser -h "$HOME" -u "$(id -u)" -G "$(id -gn)" -s /bin/ash rabbitmq
 fi
 
-echo "$RABBITMQ_ERLANG_COOKIE" > $HOME/.erlang.cookie
-chmod 0600 $HOME/.erlang.cookie
+echo "$RABBITMQ_ERLANG_COOKIE" > "$HOME/.erlang.cookie"
+chmod 0600 "$HOME/.erlang.cookie"
+
+if
+  [[ -z $RABBITMQ_NODENAME ]] && \
+  [[ -n $RABBITMQ_NODENAME_PREFIX ]] && \
+  [[ -n $MY_POD_IP ]]
+then
+  export RABBITMQ_NODENAME="${RABBITMQ_NODENAME_PREFIX}@${MY_POD_IP}"
+fi
 
 docker-entrypoint.sh rabbitmq-server
